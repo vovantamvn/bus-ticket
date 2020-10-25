8d2d7fae16e1d2f49640626c667cc717b4eca9c1
Võ Văn Tâm <vovantam.dev@gmail.com>
2020-10-25 14:52:05 +0700

    Add template (#1)

---
+++ src/main/java/com/spring/app/controller/AbstractController.java
@@ -0,0 +1,7 @@
+package com.spring.app.controller;
+
+public class AbstractController {
+    public static final String REDIRECT = "redirect:/";
+    public static final String WEB = "site/web/";
+    public static final String ADMIN = "site/admin/";
+}
