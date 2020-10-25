b10890a4b4cb0d80fe4fcf190adb919c26b29b13
Vo Van Tam <vovantam.dev@gmail.com>
2020-10-25 14:50:25 +0700

    Add template

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 Trang web bán vé xe buýt
 
+## Cách cài đặt môi trường
+
+- Cài JDK 11 trở lên
+- Cài MySQL
+>
+> - Đổi mật khẩu user root thành root
+> - Tạo một database có tên là db_db_bus_tickets
+> - Nếu MySQL chạy ở port khác 3306 thì config về 3306
+>
+
 ## Cách chạy project
 
+- Mở project lên và gõ: ./mvnw spring-boot:run
+- Truy cập vào http://localhost:8080
 ## Các thành viên trong nhóm
 - Võ Văn Tâm
 - Hồ Văn Anh Kim
--- src/main/java/com/spring/app/controller/user/HomeController.java
+++ src/main/java/com/spring/app/controller/user/HomeController.java
@@ -1,14 +1,23 @@
 package com.spring.app.controller.user;
 
+import com.spring.app.controller.AbstractController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
-public class HomeController {
+public class HomeController extends AbstractController {
+    private static final String PAGE = WEB + "index";
+
     @GetMapping("/")
     public ModelAndView getIndex() {
-        ModelAndView modelAndView = new ModelAndView("web/index");
+        ModelAndView modelAndView = new ModelAndView(PAGE);
+
+        List<String> names = List.of("1", "2", "3", "4");
+        modelAndView.addObject("names", names);
+
         return modelAndView;
     }
 
